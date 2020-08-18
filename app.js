ec17aae707a156882c9a1c44feb59fbf187b4351
Nazia-Islam <naziasebelly@gmail.com>
2020-08-18 21:27:42 +0600

    clear code and single if statement

--- app.js
+++ app.js
@@ -6,18 +6,15 @@ searchButton.addEventListener('click', function(){
     document.getElementById('songItem').innerText = "";
     console.log(searchInput.value);
     fetch(`https://api.lyrics.ovh/suggest/${searchInput.value}`)
-    // fetch(`http://api.deezer.com/search?limit=10&q=${searchInput.value}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
         const searchList = data;
-        for (let i = 0; i < 10; i++) {
+        let noOfSong = (searchList.data.length > 10) ? 10 : searchList.data.length;
+        for (let i = 0; i < noOfSong; i++) {
             const artist = searchList.data[i].artist.name;
             const songTitle = searchList.data[i].title;
             const img = searchList.data[i].artist.picture_small;
             console.log(songTitle+" Albym by "+artist);
-            // document.getElementById('songTitle').innerText = songTitle;
-            // document.getElementById('artist').innerText = artist;
             const songItem = document.getElementById('songItem');
 
             songItem.innerHTML +=   `<div class="single-result row align-items-center my-3 p-3">
@@ -38,8 +35,6 @@ function searchLyrics(artist, songTitle){
     fetch(`https://api.lyrics.ovh/v1/${artist}/${songTitle}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data);
-        console.log(data.lyrics);
         if(data.error){
             document.getElementById('current-lyrics').innerText = "Lyrics not found.";
         }
